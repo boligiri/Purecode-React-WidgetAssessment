335e9b8edb487fa0a2976c0d1c692322a7c6c293
boligiri <165356464+boligiri@users.noreply.github.com>
2024-03-29 11:08:34 +0530

    Add files via upload

---
+++ src/components/PageTitle.jsx
@@ -0,0 +1,12 @@
+// import { Helmet } from "react-helmet-async";
+
+const PageTitle = ({ title }) => {
+  return (
+
+      <title>
+        {title} | Yum Reactjs - Multipurpose Food Tailwind CSS Template
+      </title>
+  );
+};
+
+export default PageTitle;
