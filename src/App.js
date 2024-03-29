e4904a09bc7fd868a6595eb1fdffd4108e636a64
boligiri <165356464+boligiri@users.noreply.github.com>
2024-03-29 11:37:08 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import Widget from './widget/page.jsx';
+
+function App() {
+  return (
+    <>
+    <Widget />
+    </>
+  );
+}
+
+export default App;
